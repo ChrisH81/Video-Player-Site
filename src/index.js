3e6620ce206e3e804e37caf455a189feadea8af7
ChrisH81 <chrishurst1981@icloud.com>
2017-09-04 15:25:31 +0200

    remove unwanted sections

--- src/index.js
+++ src/index.js
@@ -3,12 +3,8 @@ import ReactDOM from "react-dom";
 
 const API_KEY = 'AIzaSyAmcvROkz8BFPbaUJ8T1wncaBgXIRHbm38'
 
-const App = function() {
+const App = () => {
   return <div>Hi!</div>;
-}
-
-const Yo = () => {
-  return <h1>Yo!</h1>;
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));

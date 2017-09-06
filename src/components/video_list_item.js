eb3162fae1e5d78288448c2109bfb89b2e310e46
ChrisH81 <chrishurst1981@icloud.com>
2017-09-06 19:15:42 +0200

    refactoring video list items

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const VideoListItem = ({video}) => {
-  const imageURL = video.snippet.thumbnails.default.url;
+  const imageUrl = video.snippet.thumbnails.default.url;
 
   return (
     <li className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
-          <img className="media-object" src={imageURL}/>
+          <img className="media-object" src={imageUrl}/>
         </div>
         <div className="media-body">
           <div className="media-heading">{video.snippet.title}</div>
